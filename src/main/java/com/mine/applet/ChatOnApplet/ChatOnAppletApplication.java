b6d79b598161241cf7cdf8962a699f8b8ae5d504
yerongfa <1040051280@qq.com>
2019-02-14 16:26:10 +0800

    上传项目至github

---
+++ src/main/java/com/mine/applet/ChatOnApplet/ChatOnAppletApplication.java
@@ -0,0 +1,14 @@
+package com.mine.applet.ChatOnApplet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatOnAppletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatOnAppletApplication.class, args);
+	}
+
+}
+
